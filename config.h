dff70adf62e9b4dfbbdca0c0621d74d66292d524
anki <an.kiselev.2008@gmail.com>
2025-09-18 14:34:04 +0300

    filename change

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char INPUT_FILEPATH[] = "onegin_eng.txt";
+const char INPUT_FILEPATH[] = "onegin.txt";
 
 const char SORTED_BYSTART_FILEPATH[] = "sorted_by_start.txt";
 
